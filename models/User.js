82eac7215849c8f425f1edc8596334c464964b28
Victor Camarena Ballester <victor.camarena.ballester@gmail.com>
2019-09-06 22:28:24 +0200

    first models on

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    username: {
+      firstname: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true,
+      },
+      lastname: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true,
+      },
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      index: true,
+    },
+    bio: String,
+    avatar: String,
+    birthday: Date,
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+    },
+    address: {
+      street: String,
+      city: String,
+      estate: String,
+      country: String,
+      zip: Number,
+    },
+    admin: Boolean,
+    hashpass: String,
+    dni: Number,
+    tel: [Number],
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    active: Boolean,
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
