c75be76fc6cb80c68127274c1238594f8bdd8050
Victor Camarena Ballester <victor.camarena.ballester@gmail.com>
2019-09-24 17:13:21 +0200

    [feat] closed: message sockets when booking

--- public/js/bookings.js
+++ public/js/bookings.js
@@ -33,4 +33,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, '');
   initServices();
+
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, '');
 }); 
+
